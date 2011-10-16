3d6051e5f35169981ec98f1dd277fb0b6857765e
saurabhsinha <saurabh.nitc10@gmail.com>
2011-10-17 02:31:50 +0530

    index.php title

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE>
 <html>
 	<head>
-		<title>Andromeda</title>
+		<title>Sockmonkey</title>
 			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 			<link rel="stylesheet" href="css/sockmonkey.css" type="text/css" charset="utf-8"/>
 					<script type="text/javascript" src="js/sockmonkey.js"></script>
-			<script type="text/javascript" src="js/jquery.min.js"></script>
+			<script type="text/javascript" src=""></script>
 	</head>
 	<body>
 	</body>
