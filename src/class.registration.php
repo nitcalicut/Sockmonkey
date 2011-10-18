4636d42f48e7ecb0d9b640890575d8036f883fde
saurabh <indranil@indranil-PC.(none)>
2011-10-19 04:44:54 +0530

    beforeConfirmationOk

--- src/class.registration.php
+++ src/class.registration.php
@@ -28,13 +28,13 @@ class registration
 	public function __destruct() { }
 
 	protected function view($x){
-		$sql="Select * from registration where rg_teamid = '$x'";
+		$sql="SELECT rg_no,rg_eventid,rg_teamid,rg_captainid,rg_captaincnfrm,rg_part1,rg_cnfrm1,rg_part2,rg_cnfrm2,rg_part3,rg_cnfrm3,rg_part4,rg_cnfrm4,rg_part5,rg_cnfrm5,rg_part6,rg_cnfrm6 FROM registration WHERE rg_teamid = '".$x."'";
 		$user=pg_fetch_assoc(dbquery($sql));
 		$this->rgNo=$user['rg_no'];
 		$this->rgEventId=$user['rg_eventid'];
 		$this->rgTeamId=$user['rg_teamid'];
 		$this->rgCaptainId=$user['rg_captainid'];
-		$this->rgCaptainConfirm=['rg_capcnfrm'];
+		$this->rgCaptainConfirm=$user['rg_captaincnfrm'];
 		$this->rgPart1=$user['rg_part1'];
 		$this->rgConfirm1=$user['rg_cnfrm1'];
 		$this->rgPart2=$user['rg_part2'];
@@ -50,7 +50,7 @@ class registration
 		
 	}
 
-	public function create($rgEventId,$rgTeamId,$rgCaptainId,$rgPart1,$rgPart2,$rgPart3,$rgPart4,$rgPart5,$rgPart6,$rgPart7){
+	protected function create($rgEventId,$rgTeamId,$rgCaptainId,$rgPart1,$rgPart2,$rgPart3,$rgPart4,$rgPart5,$rgPart6,$rgPart7){
 		$this->rgEventId	=	pg_escape_string($rgEventId);
 		$this->rgTeamId		=	pg_escape_string($rgTeamId);
 		$this->rgCaptainId	=	pg_escape_string($rgCaptainId);
@@ -69,7 +69,7 @@ class registration
 										rg_part3,
 										rg_part4,
 										rg_part5,
-										rg_part6,) 
+										rg_part6) 
 							values ('".$this->rgEventId."',
 									'".$this->rgTeamId."',
 									'".$this->rgCaptainId."',
@@ -78,13 +78,13 @@ class registration
 									'".$this->rgPart3."',
 									'".$this->rgPart4."',
 									'".$this->rgPart5."',
-									'".$this->rgPart6."',) returning rg_no";
+									'".$this->rgPart6."') returning rg_no";
 		$user=pg_fetch_assoc(dbquery($sql));
 		$this->rgNo=$user['rg_no'];
 	}
 
 	public function getRgNo(){
-		Return $this->rgNo;
+		return $this->rgNo;
 	}
 
 	public function getRgEventId(){
