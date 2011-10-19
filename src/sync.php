a12fe442eb3dd40fc92adbe116d5e70bb47d9355
Vipin Nair <swvist@gmail.com>
2011-10-19 11:39:28 +0530

    Modified for the database design change

--- src/sync.php
+++ src/sync.php
@@ -9,7 +9,7 @@
 		$sql = "Select * from participant";
 		$res = myquery($sql);
 		while($user = mysql_fetch_assoc($res)){
-			$sql = "Insert into participant (pc_tatid, pc_name, pc_email, pc_college, pc_contact, pc_state, pc_gender, pc_accomreqst) values('".pg_escape_string($user['tathva_id'])."','".pg_escape_string($user['name'])."','".pg_escape_string($user['email'])."','".pg_escape_string($user['college_name'])."','".pg_escape_string($user['phone'])."','".pg_escape_string($user['state'])."','','n');";
+			$sql = "Insert into participant (pc_tatid, pc_name, pc_email, pc_college, pc_contact, pc_state, pc_gender, pc_accomreqst) values('".pg_escape_string($user['tathva_id'])."','".pg_escape_string($user['name'])."','".pg_escape_string($user['email'])."','".pg_escape_string($user['college_name'])."','".pg_escape_string($user['phone'])."','".pg_escape_string($user['state'])."','','n');<br />";
 			echo $sql;
 		}
 	}
@@ -47,7 +47,7 @@
 			'".pg_escape_string($event["max_part"])."',
 			'',
 			'',
-			'',0);";
+			'',0)<br />;";
 			echo $sql;
 		}
 	}
@@ -62,6 +62,7 @@
 		while($row=mysql_fetch_array($res)){
 			//echo $row[0]." | ".$row[1]." | ".$row[2]." | ".$team[1]."<br />";
 			$team = explode(",",$row[3]);
+			array_splice($team,array_search($row[2],$team),1);
 			$sql = "Insert into registration(rg_eventid,
 											rg_teamid,
 											rg_captainid,
@@ -70,8 +71,7 @@
 											rg_part3,
 											rg_part4,
 											rg_part5,
-											rg_part6,
-											rg_part7)
+											rg_part6)
 									values('".pg_escape_string(strtoupper($row[0]))."',
 											'".pg_escape_string(strtoupper($row[1]))."',
 											'".pg_escape_string(strtoupper($row[2]))."',
@@ -80,8 +80,7 @@
 											'".pg_escape_string(strtoupper($team[2]))."',
 											'".pg_escape_string(strtoupper($team[3]))."',
 											'".pg_escape_string(strtoupper($team[4]))."',
-											'".pg_escape_string(strtoupper($team[5]))."',
-											'".pg_escape_string(strtoupper($team[6]))."');";
+											'".pg_escape_string(strtoupper($team[5]))."');<br />";
 			echo $sql;
 		}
 	}
