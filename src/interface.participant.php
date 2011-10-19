37144338043d8b6cdc9af5204538e43b47fecb1b
Vipin Nair <swvist@gmail.com>
2011-10-19 14:13:21 +0530

    Added participant confirmation function

--- src/interface.participant.php
+++ src/interface.participant.php
@@ -1,6 +1,12 @@
 <?php
-	include 'class.participant.php';
-	function userSearch($arg){
+	include_once 'class.participant.php';
+	include_once 'class.registration.php';
+	function participantSearch($arg){
 		return participant::search($arg);
+	}
+	function participantConfirm($tid){
+		$p = new participant($tid);
+		$p->updateStatus("Y");
+		registration::confirmEventParticipation($tid);
 	}
 ?>
