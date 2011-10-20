8597c52dab8d3a9ee68627a60f7cf49a1f9c8aa0
Jaseem Abid <jaseemabid@gmail.com>
2011-10-21 01:30:26 +0530

    Sample UI stuff

---
+++ samples.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Sockmonkey</title>
+
+<?php
+	include 'source.php';
+	echo $header;
+?>
+
+</head>
+<body>
+<a href='/Sockmonkey'><div id='topbar'></div></a>
+<div id='container'>
+<?php
+	echo $topBar;
+?>
+
+<form id="searchBox">
+	<input type="search" value="" />
+	<span>Search<span>
+</form>
+
+<nav>
+	<ul id="mainMenu">
+		<li><a>Home</a></li>
+		<li><a>About me</a></li>
+		<li><a>Stuff i do</a></li>
+		<li><a>Stuff i love</a></li>
+		<li><a>Contact</a></li>
+	</ul>
+</nav>
+
+</div><!-- /container -->
+<?php
+	echo $bottomBar;
+	echo $scripts;
+?>
+</body>
+</html>
+
