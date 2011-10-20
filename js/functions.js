44d1dfd6c5b50a29f1452236825bb045187e2f7e
Jaseem Abid <jaseemabid@gmail.com>
2011-10-21 04:08:25 +0530

    Graout, search template update, pop()

--- js/functions.js
+++ js/functions.js
@@ -30,6 +30,9 @@ if(typeof String.prototype.supplant !== 'function') {
 }
 
 var sockmonkey = {
+	grayOut : function() {
+		function grayOut(vis,options){var options=options||{},zindex=options.zindex||99,opacity=options.opacity||50,opaque=(opacity/70),bgcolor=options.bgcolor||'#030303',dark=document.getElementById('darkenScreenObject');if(!dark){var tbody=document.getElementsByTagName("body")[0],tnode=document.createElement('div');tnode.style.position='fixed';tnode.style.top='0px';tnode.style.left='0px';tnode.style.overflow='hidden';tnode.style.display='none';tnode.id='darkenScreenObject';tbody.appendChild(tnode);dark=document.getElementById('darkenScreenObject');}if(vis){if(document.body&&(document.body.scrollWidth||document.body.scrollHeight)){var pageWidth='100%';var pageHeight='2000px';}dark.style.opacity=opaque;dark.style.MozOpacity=opaque;dark.style.filter='alpha(opacity='+opacity+')';dark.style.zIndex=zindex;dark.style.backgroundColor=bgcolor;dark.style.width=pageWidth;dark.style.height=pageHeight;dark.style.display='block';}else{dark.style.display='none';}}
+	},
 	getUsers : function(Name) {
 			$.getJSON( 'src/response.participant.php?search='+Name , function(json) {
 				$('div#searchResults').html('');
@@ -49,16 +52,22 @@ var sockmonkey = {
 				$('input#search').attr('value', getUrlVars()['search']);
 				sockmonkey.getUsers(getUrlVars()['search']);
 			}
-		}
+		},
+	Pop : function(json) {
+		grayOut('true');
+		$('div#pop').show()
+		$('div#pop').html(sockmonkey.templates.Search.supplant(json));
+	}
 };
 
 sockmonkey.templates = { 
-	Search : "<span class='name'>{pc_name}</span><br /><span class='tid'>{pc_tatid} : </span><span class='tid'>{pc_confirm}</span><br /><span class='contact'>email : {pc_email} </span><br /><span class='contact'>College :</span><span class='contact'>{pc_college}</span><br /><br />"
+	Search : "<span class='name'>{pc_name}</span><br /><span class='tid'>{pc_tatid}</span><br/><span class='tid'>Status : </span><span class='tid'>{pc_confirm}</span><br /><span class='contact'>email : {pc_email} </span><br /><span class='contact'>College :</span><span class='contact'>{pc_college}</span><br /><br />"
 };
 
 
 $(document).ready(function(){
 	sockmonkey.Search();
+	sockmonkey.grayOut()
 });
 
 
