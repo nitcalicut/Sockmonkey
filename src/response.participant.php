64e57aa09984ec8e442f7763c4fbb43bb8b4a1c3
saurabh <indranil@indranil-PC.(none)>
2011-10-20 15:39:44 +0530

    after create user response

--- src/response.participant.php
+++ src/response.participant.php
@@ -19,5 +19,16 @@
 	if(isset($_GET['participantinfo'])){
 		echo json_encode(participantInfo($_GET['participantinfo']));
 	}
+	if(isset($_GET['pid'])&&
+		isset($_GET['pname'])&&
+		isset($_GET['pemail'])&&
+		isset($_GET['pclg'])&&
+		isset($_GET['pcntct'])&&
+		isset($_GET['pstate'])&&
+		isset($_GET['pgender'])&&
+		isset($_GET['preq'])&&
+		isset($_GET['pnitc'])){
+		newUser($_GET['pid'],$_GET['pname'],$_GET['pemail'],$_GET['pclg'],$_GET['pcntct'],$_GET['pstate'],$_GET['pgender'],$_GET['preq'],$_GET['pnitc']);
+	}
 	
 ?>
