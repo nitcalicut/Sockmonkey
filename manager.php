09f28bcd62b0cb493a52aa2b32f5e650f5b331fb
Jaseem Abid <jaseemabid@gmail.com>
2011-10-21 01:41:39 +0530

    The ui page for manager

---
+++ manager.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Sockmonkey</title>
+
+<?php
+	include 'source.php';
+	echo $header;
+?>
+
+</head>
+<body>
+<a href='/Sockmonkey'><div id='topbar'></div></a>
+<div id='container'>
+<?php
+	echo $topBar;
+?>
+</div><!-- /container -->
+<?php
+	echo $bottomBar;
+	echo $scripts;
+?>
+</body>
+</html>
+
