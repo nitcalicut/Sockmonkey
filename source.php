26697b6ba8021a7dc94e5222b143fb4129f1f619
Jaseem Abid <jaseemabid@gmail.com>
2011-10-20 22:10:24 +0530

    Source of all evil :)

---
+++ source.php
@@ -0,0 +1,15 @@
+<?php
+
+$header="
+	<meta charset='UTF-8' />
+	<link rel='shortcut icon' href='pics/favicon.png'>
+	<link rel='stylesheet' href='css/structure.css'/>
+	<link rel='stylesheet' href='css/popup.css'/>
+	<link rel='stylesheet' href='css/storybox.css'/>";
+
+$scripts="
+	<script src='js/jquery-1.6.2.min.js' type='text/javascript'></script>
+	<script src='js/functions.js' type='text/javascript' ></script>";
+
+?>
+
