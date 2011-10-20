0c7d3256998a29e53123ac85a8d044a0ef12c82c
Jaseem Abid <jaseemabid@gmail.com>
2011-10-20 22:12:57 +0530

    Crude first template

---
+++ template.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Sockmonkey</title>
+
+<?php
+	include 'source.php';
+	echo $header;
+?>
+
+</head>
+<body>
+<div id='topbar'></div>
+<div id='container'>
+<?php
+	echo $topBar;
+	echo $feedback; 
+?>
+</div><!-- /container -->
+<?php
+	echo $bottomBar;
+	echo $scripts;
+?>
+</body>
+</html>
+
