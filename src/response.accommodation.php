2c46addf5bf35e508249fc6f5a4b2423403ad0d2
saurabh <indranil@indranil-PC.(none)>
2011-10-20 12:39:56 +0530

    responseAccomOk

---
+++ src/response.accommodation.php
@@ -0,0 +1,23 @@
+<?php
+	
+	include_once 'interface.accommodation.php';
+	
+	if(isset($_POST['captid'])&&
+		isset($_POST['items'])&&
+		isset($_POST['room'])&&
+		isset($_POST['team'])){
+			accommodationCreateTeam($_POST['captid'],$_POST['items'],$_POST['room'],$_POST['team']);
+	}
+
+	if(isset($_POST['oldcapt'])&&
+		isset($_POST['newcapt'])&&
+		isset($_POST['items'])&&
+		isset($_POST['room'])&&
+		isset($_POST['dereg'])){
+			accommodationUpdateCapt($_POST['oldcapt'],$_POST['newcapt'],$_POST['items'],$_POST['room'],$_POST['dereg']);
+	}
+	
+	if(isset($_POST['captid'])){
+		accommodationDeleteTeam($_POST['captid']);
+	}
+?>
