fc8b3ed7e08bb0de2fb0b88cadcd7a07277f8668
Jaseem Abid <jaseemabid@gmail.com>
2011-10-20 23:12:28 +0530

    less library

--- source.php
+++ source.php
@@ -4,7 +4,7 @@ $header="
 	<meta charset='UTF-8' />
 	<link rel='shortcut icon' href='pics/favicon.png'>
 	<link rel='stylesheet/less' type='text/css' href='css/styles.less'>
-	<script src='js/less.js' type='text/javascript'></script>";
+	<script src='js/less-1.1.3.min.js' type='text/javascript'></script>";
 
 $scripts="
 	<script src='js/jquery-1.6.2.min.js' type='text/javascript'></script>
