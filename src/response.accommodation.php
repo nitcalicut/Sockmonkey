0ed210e4f3a0ce400640d30353c51c74b1f727b8
Vipin Nair <swvist@gmail.com>
2011-10-20 12:50:47 +0530

    Added else case

--- src/response.accommodation.php
+++ src/response.accommodation.php
@@ -8,17 +8,17 @@
 		isset($_POST['team'])){
 			accommodationCreateTeam($_POST['captid'],$_POST['items'],$_POST['room'],$_POST['team']);
 	}
-
-	if(isset($_POST['oldcapt'])&&
+	else if(isset($_POST['oldcapt'])&&
 		isset($_POST['newcapt'])&&
 		isset($_POST['items'])&&
 		isset($_POST['room'])&&
 		isset($_POST['dereg'])){
 			accommodationUpdateCapt($_POST['oldcapt'],$_POST['newcapt'],$_POST['items'],$_POST['room'],$_POST['dereg']);
 	}
-	
-	if(isset($_POST['captid'])){
+	else if(isset($_POST['captid'])){
 		accommodationDeleteTeam($_POST['captid']);
 	}
-	echo json_encode(listAccommodationData());
+	else{
+		echo json_encode(listAccommodationData());
+	}
 ?>
