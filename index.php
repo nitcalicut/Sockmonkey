c9293fc4fe6f2c5ebda54c75e734329016ce040e
Vipin Nair <swvist@gmail.com>
2011-10-22 04:13:48 +0530

    Hid login when logged in and hid link when logged out

--- index.php
+++ index.php
@@ -15,32 +15,38 @@
 <?php
 	echo $topBar;
 ?>
-
-	<h1>QUICK LINKS</h1>
-	<ul style="font-size:20px;">
-		<li><a href="register.php">Register offline</a></li>
-		<li><a href="manager.php">Search & Managers page</a></li>
-		<li><a href="details.php">User details page</a></li>
-		<li><a href="accommodation.php">Accommodation</a></li>
-		<li><a href="eventregn.php">Event regn</a></li>
-		<li><a href="logout.php">logout</a></li>
-	</ul>
-
-<div class="g">
-	<h2>Sign in <strong></strong></h2>
-	<form method="post" action="login.php">
-<label>
-	<strong class="email-label">Username</strong>
-	<input type="text" value="" id="Email" name="email">
-</label>
-<label>
-	<strong class="passwd-label">Password</strong>
-	<input type="password" id="Passwd" name="password">
-</label>
-	<input type="submit" value="Sign in" name="signIn" class="g-button">
-	</form>
-</div>
-
+<?php 
+	session_start();
+	if(isset($_SESSION['user'])){
+		echo 
+		"<h1>QUICK LINKS</h1>
+		<ul style='font-size:20px;'>
+			<li><a href='register.php'>Register offline</a></li>
+			<li><a href='manager.php'>Search & Managers page</a></li>
+			<li><a href='details.php'>User details page</a></li>
+			<li><a href='accommodation.php'>Accommodation</a></li>
+			<li><a href='eventregn.php'>Event regn</a></li>
+			<li><a href='logout.php'>logout</a></li>
+		</ul>";
+	}
+	else{
+	echo "
+	<div class='g'>
+		<h2>Sign in <strong></strong></h2>
+		<form method='post' action='login.php'>
+	<label>
+		<strong class='email-label'>Username</strong>
+		<input type='text' value='' id='Email' name='email'>
+	</label>
+	<label>
+		<strong class='passwd-label'>Password</strong>
+		<input type='password' id='Passwd' name='password'>
+	</label>
+		<input type='submit' value='Sign in' name='signIn' class='g-button'>
+		</form>
+	</div>";
+	}
+?>
 </div><!-- /container -->
 <?php
 	echo $bottomBar;
