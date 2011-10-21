0df1064a255ca83b87c64e220273bbcc71dd1a7c
Jaseem Abid <jaseemabid@gmail.com>
2011-10-21 06:02:52 +0530

    Complete user details

---
+++ details.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Sockmonkey</title>
+
+<?php
+	include 'source.php';
+	echo $header;
+?>
+
+</head>
+<body>
+<a href='/Sockmonkey'><div id='topbar'></div></a>
+<div id='container'>
+<?php
+	echo $topBar;
+?>
+
+<div id="details"></div>
+<div id="details2"></div>
+
+</div>
+
+
+
+</div><!-- /container -->
+<?php
+	echo $bottomBar;
+	echo $scripts;
+?>
+</body>
+</html>
+
