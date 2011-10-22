dca4de739f6ebac3f18b9ec01c517d42961593a3
Jaseem Abid <jaseemabid@gmail.com>
2011-10-22 13:09:28 +0530

    Shows teamlist for an event

--- teamlist.php
+++ teamlist.php
@@ -1,5 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Sockmonkey</title>
+
 <?php
 	include 'src/interface.php';
+	include 'source.php';
+	echo $header;
+	session_start();
+	if($_SESSION['user']!="manager")
+		header("Location: index.php");
+?>
+
+
+</head>
+<body>
+<a href='/Sockmonkey'><div id='topbar'></div></a>
+<div id='container'>
+<?php
+	echo $topBar;
+
 	$arr=listEventTeams($_GET['eid']);
 	$i=1;
 	echo "<table border=\"1\">";
@@ -9,4 +29,23 @@
 		 $i++;
 	}
 	echo "</table>";
+
 ?>
+
+<form id="searchBox" action="">
+	<input type="search" id="search" placeholder="Enter the event ID" name="eid"/>
+	<span id="SearchButton">Search<span>
+</form>
+
+<div id="searchResults">
+
+</div>
+
+
+</div><!-- /container -->
+<?php
+	echo $bottomBar;
+	echo $scripts;
+?>
+</body>
+</html>
