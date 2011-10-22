8704e46553cd42a3d86e65bcabd4417104eea160
Jaseem Abid <jaseemabid@gmail.com>
2011-10-22 12:06:23 +0530

    Removed unwanted junk

--- js/functions.js
+++ js/functions.js
@@ -30,9 +30,6 @@ if(typeof String.prototype.supplant !== 'function') {
 }
 
 var sockmonkey = {
-	grayOut : function() {
-		return function grayOut(vis,options){var options=options||{},zindex=options.zindex||99,opacity=options.opacity||50,opaque=(opacity/70),bgcolor=options.bgcolor||'#030303',dark=document.getElementById('darkenScreenObject');if(!dark){var tbody=document.getElementsByTagName("body")[0],tnode=document.createElement('div');tnode.style.position='fixed';tnode.style.top='0px';tnode.style.left='0px';tnode.style.overflow='hidden';tnode.style.display='none';tnode.id='darkenScreenObject';tbody.appendChild(tnode);dark=document.getElementById('darkenScreenObject');}if(vis){if(document.body&&(document.body.scrollWidth||document.body.scrollHeight)){var pageWidth='100%';var pageHeight='2000px';}dark.style.opacity=opaque;dark.style.MozOpacity=opaque;dark.style.filter='alpha(opacity='+opacity+')';dark.style.zIndex=zindex;dark.style.backgroundColor=bgcolor;dark.style.width=pageWidth;dark.style.height=pageHeight;dark.style.display='block';}else{dark.style.display='none';}}
-	},
 	getUsers : function(Name) {
 			$.getJSON( 'src/response.participant.php?search='+Name , function(json) {
 				$('div#searchResults').html('');
@@ -53,15 +50,6 @@ var sockmonkey = {
 				sockmonkey.getUsers(getUrlVars()['search']);
 			}
 		},
-	Pop : function(json) {
-		$('div#searchResults span.name').click(function(){
-			sockmonkey.grayOut()('true');
-			$('div#pop').show()
-			$('div#pop').html(sockmonkey.templates.Search.supplant(json));
-			n  = this.parent().children('span.tid').text();
-			console.log(n);
-		});
-	},
 	participantinfo : function(tid) {
 		$.getJSON( 'src/response.participant.php?participantinfo='+tid , function(json) {
 			$('div#searchResults').html('');
@@ -122,8 +110,6 @@ sockmonkey.templates = {
 
 $(document).ready(function(){
 	sockmonkey.Search();
-	sockmonkey.grayOut();
-	sockmonkey.Pop();
 	if(getUrlVars()['tid']) {
 		$('input#search').attr('value', getUrlVars()['tid']);
 		sockmonkey.details(getUrlVars()['tid']);
